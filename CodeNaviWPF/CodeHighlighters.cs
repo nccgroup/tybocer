4c6110a5a7b76e731807b029b9d5a2af21b2caac
Felix Ingram <f.ingram@gmail.com>
2013-08-11 23:38:26 +0100

    Add CodeHighlighter

---
+++ CodeNaviWPF/CodeHighlighters.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Released as open source by NCC Group Plc - http://www.nccgroup.com/
+ * 
+ * Developed by Felix Ingram, (felix.ingram@nccgroup.com)
+ * 
+ * http://www.github.com/nccgroup/tybocer
+ * 
+ * Released under AGPL. See LICENSE for more information
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeNaviWPF
+{
+    class CodeHighlighters
+    {
+    }
+}
